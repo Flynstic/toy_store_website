3cc642e1c02ba5e09368372befe868e3f9294cfe
AbdulazizMohammad <opomhmd@hotmail.com>
2024-03-14 23:38:56 +0300

    add views

--- apps/toymodule/views.py
+++ apps/toymodule/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, redirect
 def index(request):
     # render the appropriate template for this request
     return render(request, 'toymodule/index.html')
+
+def babyToys(request):
+    return render(request, 'toymodule/babyToys.html')
+
+def outdoors(request):
+    return render(request, 'toymodule/outdoors.html')
+
+def dollsAndPlaysets(request):
+    return render(request, 'toymodule/dollsAndPlaysets.html')
+
+def carsAndBikes(request):
+    return render(request, 'toymodule/carsAndBikes.html')
