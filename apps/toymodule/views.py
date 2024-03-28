4d0a6e7362a7f787d8d8a21ab2d7140c9e971766
AbdulazizMohammad <opomhmd@hotmail.com>
2024-03-29 00:32:35 +0300

    new view configs

--- apps/toymodule/views.py
+++ apps/toymodule/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render, redirect
+from .models import User, Product
 
 def index(request):
     # render the appropriate template for this request
-    return render(request, 'toymodule/index.html')
+    products = Product.objects.filter()
+    return render(request, 'toymodule/index.html', {'products':products})
 
 def login(request):
     return render(request, 'toymodule/login.html')
 
+def authorize(request):
+    pass
+
 def register(request):
+    if request.method == "POST":
+        mail = request.POST.get('email')
+        passwd = request.POST.get('passwd')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        print(mail, passwd)
+        newuser = User.objects.create(email = mail, password = passwd, firstname = fname, lastname= lname)
+        newuser.save()
+        return render(request, 'toymodule/registerSuccess.html')
+    
     return render(request, 'toymodule/register.html')
 
 def babyToys(request):
-    return render(request, 'toymodule/babyToys.html')
+    products = Product.objects.filter(pcategory__exact = 'Baby Toys')
+    return render(request, 'toymodule/babyToys.html', {'products':products})
 
 def outdoors(request):
-    return render(request, 'toymodule/outdoors.html')
+    products = Product.objects.filter(pcategory__exact = 'Outdoors')
+    return render(request, 'toymodule/outdoors.html', {'products':products})
 
 def dollsAndPlaysets(request):
-    return render(request, 'toymodule/dollsAndPlaysets.html')
+    products = Product.objects.filter(pcategory__exact = 'Dolls and Playsets')
+    return render(request, 'toymodule/dollsAndPlaysets.html', {'products':products})
 
 def carsAndBikes(request):
-    return render(request, 'toymodule/carsAndBikes.html')
+    products = Product.objects.filter(pcategory__exact = 'Cars and Bikes')
+    return render(request, 'toymodule/carsAndBikes.html', {'products':products})
