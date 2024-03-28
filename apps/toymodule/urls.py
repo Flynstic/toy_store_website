2b64e8c05045a874bcedb8054423cd81e9bd46f4
AbdulazizMohammad <opomhmd@hotmail.com>
2024-03-29 00:32:59 +0300

    add media url

--- apps/toymodule/urls.py
+++ apps/toymodule/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, re_path
 from apps.toymodule import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [    
     path('', views.index, name='index'),
@@ -9,4 +12,4 @@ urlpatterns = [
     path('cars-and-bikes', views.carsAndBikes),
     path('login', views.login),
     path('register', views.register),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
