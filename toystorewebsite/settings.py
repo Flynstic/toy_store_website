46a1f8a51fce60b046bc5cae4443df10e04a981c
AbdulazizMohammad <opomhmd@hotmail.com>
2024-03-29 00:34:17 +0300

    modify media path

--- toystorewebsite/settings.py
+++ toystorewebsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.toymodule',
     'bootstrap5',
 ]
 
@@ -121,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
