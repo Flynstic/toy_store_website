04aa2f718dd13439141f10b5206afd46df641677
AbdulazizMohammad <opomhmd@hotmail.com>
2024-05-18 21:42:17 +0300

    add new urls

--- apps/toymodule/urls.py
+++ apps/toymodule/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path('outdoors', views.outdoors),
     path('dolls-and-playsets', views.dollsAndPlaysets),
     path('cars-and-bikes', views.carsAndBikes),
-    path('login', views.login),
+    path('login', views.login, name='login'),
     path('register', views.register),
-    path('addProduct', views.register),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/account-info', views.accountInfo, name='account-info'),
+    path('addProduct', views.addProduct, name='addProduct'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
